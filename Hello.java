a91bbee16351d25b1976eba5a9ce63db58f29145
skill208 <skill208@naver.com>
2017-03-29 11:57:01 +0900

    Hello.java and README.md

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Hello{
+	
+	public static void main(String args){
+		System.out.printf("hello");
+		
+	}
+
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test_repo2
 test repository한글
+this java
